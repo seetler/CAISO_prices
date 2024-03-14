14f08cddfd1c7df9917f1dd145f8df4e4a2eae20
Jason Zeng <jsnzeng@gmail.com>
2024-03-13 20:50:15 -0700

    Added Staging_Query

--- variables2.py
+++ variables2.py
@@ -13,9 +13,9 @@ def function_formatted_time(init_time):
   return init_time.strftime("%Y%m%dT%H:%M-0000")
 
 oasis_api_string_url='http://oasis.caiso.com/oasisapi/SingleZip?'
-oasis_api_string_queryname='PRC_HASP_LMP'
+oasis_api_string_queryname='PRC_LMP'
 oasis_api_string_startdatetime=function_formatted_time(minus_hour)
 oasis_api_string_enddatetime=function_formatted_time(plus_hour)
-oasis_api_string_marketid='HASP'
+oasis_api_string_marketid='DAM'
 oasis_api_string_node='BAYSHOR2_1_N001'
 syspathlocal='C:\\Users\\JSNZE\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python312\\site-packages'
