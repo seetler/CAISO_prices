0e74ff0114f68b2321c3d19b9c50c8c39d75a114
Jason Zeng <jsnzeng@gmail.com>
2024-04-18 14:21:59 -0700

    Misspelled EMBRCDR_2_N104 in variables3.py

--- variables3.py
+++ variables3.py
@@ -17,5 +17,5 @@ oasis_api_string_queryname='PRC_LMP'
 oasis_api_string_startdatetime=function_formatted_time(minus_time)
 oasis_api_string_enddatetime=function_formatted_time(plus_time)
 oasis_api_string_marketid='DAM'
-oasis_api_string_node='BEMBRCDR_2_N104,STATIN-L_7_N001,CATALYST_7_N002'
+oasis_api_string_node='EMBRCDR_2_N104,STATIN-L_7_N001,CATALYST_7_N002'
 syspathlocal='C:\\Users\\JSNZE\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python312\\site-packages'
